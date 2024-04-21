81a7c11b850d453ec73affea5de28f21b7d016ee
ClaudioPerrone <cla-agadir97@hotmail.it>
2024-04-21 17:37:11 +0200

    Feat: added first part of milestone 3

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -31,17 +31,26 @@
 				}
 
 				return new URL(`../assets/images/${flagImageName}`, import.meta.url).href;
+			},
+			
+			getPosterUrl() {
+				let posterImage =  this.cardInfo.poster_path;
+
+				return new URL(`https://image.tmdb.org/t/p/w154/${posterImage}`).href;
 			}
 		}
 	}
+
+
 </script>
 
 <template>
 	<ul>
+		<li><img :src="getPosterUrl()"></li>
 		<li><h3>Titolo: "{{ cardInfo.title }} {{ cardInfo.name }}"</h3></li>
 		<li><h2>Titolo originale: "{{ cardInfo.original_title }} {{ cardInfo.name }}"</h2></li>
 		<li>
-			<p>Lingua: <img :src="getFlagUrl()"></p>
+			<p>Lingua: <img class="flag" :src="getFlagUrl()"></p>
 		</li>
 		<li><p>Voto: {{ cardInfo.vote_average }}</p></li>
 	</ul>
@@ -55,7 +64,7 @@
 			display: inline;
 		}
 		
-		img {
+		.flag {
 			width: 25px;
 		}
 	}
